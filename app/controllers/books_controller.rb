5ca87f78bf407617695486918f7ba461baf5c8c4
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-20 23:23:06 +0900

    add:controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,46 @@
 class BooksController < ApplicationController
-  def index
-  end
 
-  def show
+  def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id]) 
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book.id)  
+    else
+      @books = Book.all
+      render action: :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id]) 
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    if book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end

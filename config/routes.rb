5b968d9ae287a720267bfa8fd3e30921dcfc6cd1
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-20 23:30:19 +0900

    add:ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+  get 'books' => 'books#index', as:'index_book'
+  get 'books/show' => 'books#show'
+  get 'books/edit' => 'books#edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show' , as: 'book'
 end
